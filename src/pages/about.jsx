36792f2e5caffd15538a3c0519301e1454b9cd58
ErickMachado95 <machado.m.erick@gmail.com>
2022-03-26 07:41:25 -0500

    Finished gatsby tutorial part 2

---
+++ src/pages/about.jsx
@@ -0,0 +1,18 @@
+import * as React from 'react'
+
+
+
+const AboutPage =  () => { 
+
+  return ( 
+    <main>
+      <title>About Page</title>
+      <h1>Learn about me!</h1>
+      <p> Hello! My name is Erick and i'm currently trying to learn about Gatsby </p>
+    </main>
+  )
+
+}
+
+
+export default AboutPage;
